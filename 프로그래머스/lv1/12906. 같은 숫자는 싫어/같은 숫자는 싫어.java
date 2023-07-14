eba9b27b4e61db8fdee93373d590bd7348088e4c
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-07-14 14:40:22 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 74.38 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+ 
+		     Stack<Integer> st = new Stack<>();
+		    
+		    int temp = 0;
+		    for (int a : arr){
+		    	if (!st.isEmpty()) {
+		            temp = st.pop() ;
+		            if(a == temp){
+		                st.push(temp);
+		            }else{
+		                st.push(temp);
+		                st.push(a);
+		            }                
+		        } else {
+		        	 st.push(a);
+		        }
+		    }
+			int[] answer = new int[st.size()];
+		    for(int i = st.size() - 1; i >=0 ; i--) {
+		    	answer[i] = st.pop();
+		    }
+
+        return answer;
+    }
+}
