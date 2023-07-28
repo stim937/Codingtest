2c195cd59d47ffe214f61115ae23ab8e9ed24979
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-07-28 14:29:01 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 83.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i += 2){
+            list.add(i);
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
