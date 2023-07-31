af3e224b3b94811320377be1ad1ec2e338384b42
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-07-31 10:08:39 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 2; i <= n; i++){
+            if(n % i == 0){
+                while(n % i == 0){
+                    n = n / i;
+                }
+                list.add(i);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
+    }
+}
