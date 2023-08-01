78feee4d1e60fa80804ba2aba92f10db5cfeb931
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-08-01 09:28:49 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;          
+        while(true){
+            int cnt = 0;
+            for(int i = 0; i < arr.length; i++){
+                if(arr[i] >= 50 && arr[i] % 2 == 0){
+                    arr[i] = arr[i] / 2;
+                    cnt++;
+                } else if(arr[i] < 50 && arr[i] % 2 == 1){
+                    arr[i] = (arr[i] * 2) + 1;
+                    cnt++;
+                }                
+            }
+            answer++;
+            
+            if(cnt == 0){
+                answer--;
+                break;  
+            } 
+        }
+        return answer;
+    }
+}
