79f9d67f0fbc516bfc8afcb0a5f441e4c935f8d9
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-08-02 10:59:23 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
+        //최소 공배수
         int lcm = (denom1 * denom2) / gcd(denom1, denom2);
         int upper = numer1 * (lcm / denom1) + numer2 * (lcm / denom2);  
         int under = lcm;
@@ -11,6 +12,7 @@ class Solution {
         return answer;
     }
     
+    //최대 공약수
     public static int gcd(int a, int b){
         if(a % b == 0){
             return b;
