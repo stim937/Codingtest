137a5d63c7ef4e2808cdfbe06c404dcc4e2db670
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-08-02 10:02:18 +0900

    [unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {       
+        int sum = 0;
+        for(int i = 0; i < number.length(); i++){
+            sum += Integer.parseInt(number.substring(i, i + 1));
+        }
+        int answer = sum % 9;
+        return answer;
+    }
+}
