d71b027623858799456b943d696695bb59780d09
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-08-03 09:59:21 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int temp = x;
+        int sum = 0;
+        while(temp > 0){
+            sum += temp % 10;
+            temp /= 10;
+        }
+        
+        if(x % sum != 0) answer = false; 
+        return answer;
+    }
+}
