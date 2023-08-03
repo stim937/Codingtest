bd1f80d3a62897c44a09255d8680b56922bdaf1d
LEE JI UI <131110537+stim937@users.noreply.github.com>
2023-08-03 10:22:46 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,16 +1,22 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;       
-        try{
-            if(s.length() == 4 || s.length() == 6){
-                int a = Integer.parseInt(s);
-            } else {
-                return false;
-            }    
-        } catch(Exception e) {
-            return false;
-        }
+        // 예외처리 활용 풀이
+        // try{
+        //     if(s.length() == 4 || s.length() == 6){
+        //         int a = Integer.parseInt(s);
+        //     } else {
+        //         return false;
+        //     }    
+        // } catch(Exception e) {
+        //     return false;
+        // }
         
+        // 정규식 활용 풀이
+        if(s.length() != 4 && s.length() != 6) answer = false;
+        s = s.replaceAll("[0-9]", "");
+        if(s.length() != 0) answer = false;
+  
         return answer;
     }
 }
